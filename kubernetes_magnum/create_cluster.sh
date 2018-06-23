c8407035db823ca4a2f3f8f793bce675546bb73f
Andrea Zonca <code@andreazonca.com>
2018-06-23 09:20:05 -0400

    create cluster

---
+++ kubernetes_magnum/create_cluster.sh
@@ -0,0 +1,14 @@
+# we can override the default values of the template
+FLAVOR="m1.medium"
+MASTER_FLAVOR=$FLAVOR
+DOCKER_VOLUME_SIZE_GB=10
+
+# number of instances
+N_MASTER=1
+N_NODES=1
+
+openstack coe cluster create --cluster-template k8s_template \
+    --master-count $N_MASTER --node-count $N_NODES \
+    --keypair comet \
+    --master-flavor $MASTER_FLAVOR --flavor $FLAVOR \
+    --docker-volume-size $DOCKER_VOLUME_SIZE_GB k8s
