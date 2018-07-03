6b95d06b36909c46baab74ac722297a0e4f9b96a
Andrea Zonca <code@andreazonca.com>
2018-07-03 19:13:16 -0400

    add also a router

--- kubernetes_magnum/create_network.sh
+++ kubernetes_magnum/create_network.sh
@@ -1,3 +1,9 @@
 openstack network create ${OS_USERNAME}-k8s-network
 
 openstack subnet create --network ${OS_USERNAME}-k8s-network --subnet-range 10.0.0.0/24 ${OS_USERNAME}-k8s-subnet1
+
+openstack router create ${OS_USERNAME}-k8s-router
+
+openstack router add subnet ${OS_USERNAME}-k8s-router ${OS_USERNAME}-k8s-subnet1
+
+openstack router set --external-gateway public ${OS_USERNAME}-k8s-router
