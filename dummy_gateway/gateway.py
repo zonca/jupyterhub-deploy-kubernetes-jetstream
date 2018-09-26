95e93f7a90a4ec81a35deda5b295196fba457ce3
Andrea Zonca <code@andreazonca.com>
2018-09-26 17:45:31 +0000

    use subprocess, s3fs does not work

--- dummy_gateway/gateway.py
+++ dummy_gateway/gateway.py
@@ -3,6 +3,8 @@ import sys
 import s3fs
 import xarray as xr
 import numpy as np
+import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -13,10 +15,14 @@ def hello():
 
 @app.route("/submit_job/<job_id>")
 def submit_job(job_id):
-    fs = s3fs.S3FileSystem(use_ssl=True, client_kwargs=dict(endpoint_url="https://tacc.jetstream-cloud.org:8080/swift/v1", region_name="RegionOne"))
-    d = s3fs.S3Map("gateway_results/{}".format(job_id), s3=fs, create=True)
-    ds = xr.DataArray(np.random.randn(100, 100))
-    ds.to_zarr(store=d, mode='w')
+    #fs = s3fs.S3FileSystem(use_ssl=True, client_kwargs=dict(endpoint_url="https://tacc.jetstream-cloud.org:8080", region_name="RegionOne"))
+    #d = s3fs.S3Map("gateway_results/{}".format(job_id), s3=fs, create=False)
+    ds = xr.Dataset({'foo': np.zeros(10), 'bar': ('x', [1, 2]), 'baz': np.pi})
+    out = "{}.nc".format(job_id)
+    ds.to_netcdf(out, mode='w')
+    subprocess.run("openstack object create dummy_gateway {}".format(out).split())
+    os.remove(out)
+
     return "Submitted job {}\n".format(job_id)
 
 if __name__ == '__main__':
