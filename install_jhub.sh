a4eb3788045d2eed0ca4a494b28f60f98a83a88e
Andrea Zonca <code@andreazonca.com>
2019-02-22 20:09:16 -0800

    update JupyterHub helm recipe to 0.8.0

--- install_jhub.sh
+++ install_jhub.sh
@@ -3,5 +3,5 @@ NAMESPACE=jhub
 
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
-      --version 0.7.0 \
+      --version 0.8.0 \
       --values config_standard_storage.yaml --values secrets.yaml
