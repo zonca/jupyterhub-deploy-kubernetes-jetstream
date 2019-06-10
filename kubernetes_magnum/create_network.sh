9cf7b4803a048f9dc8354a2c641fa341e3c2119c
Andrea Zonca <code@andreazonca.com>
2019-06-09 19:49:50 -0700

    new name

--- kubernetes_magnum/create_network.sh
+++ kubernetes_magnum/create_network.sh
@@ -1,9 +1,9 @@
-openstack network create ${OS_USERNAME}-k8s-network
+openstack network create ${OS_USERNAME}-newk8s-network
 
-openstack subnet create --network ${OS_USERNAME}-k8s-network --subnet-range 10.0.0.0/24 ${OS_USERNAME}-k8s-subnet1
+openstack subnet create --network ${OS_USERNAME}-newk8s-network --subnet-range 10.0.0.0/24 ${OS_USERNAME}-newk8s-subnet1
 
-openstack router create ${OS_USERNAME}-k8s-router
+openstack router create ${OS_USERNAME}-newk8s-router
 
-openstack router add subnet ${OS_USERNAME}-k8s-router ${OS_USERNAME}-k8s-subnet1
+openstack router add subnet ${OS_USERNAME}-newk8s-router ${OS_USERNAME}-newk8s-subnet1
 
-openstack router set --external-gateway public ${OS_USERNAME}-k8s-router
+openstack router set --external-gateway public ${OS_USERNAME}-newk8s-router
