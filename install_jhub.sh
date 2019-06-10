9c556d8004b5f743b241d98b0cafa444fc0ab8ae
Andrea Zonca <code@andreazonca.com>
2019-06-09 20:06:29 -0700

    install 0.8.2

--- install_jhub.sh
+++ install_jhub.sh
@@ -3,5 +3,5 @@ NAMESPACE=jhub
 
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
-      --version 0.8.0 \
+      --version 0.8.2 \
       --values config_standard_storage.yaml --values secrets.yaml
