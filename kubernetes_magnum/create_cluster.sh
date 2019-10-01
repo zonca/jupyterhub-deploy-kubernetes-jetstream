c0a3070e19b419e94a99f9c6128fc72258ff4bf5
Andrea Zonca <code@andreazonca.com>
2019-09-30 18:20:46 -0700

    set keypair as a variable

--- kubernetes_magnum/create_cluster.sh
+++ kubernetes_magnum/create_cluster.sh
@@ -2,6 +2,7 @@
 FLAVOR="m1.medium"
 MASTER_FLAVOR=$FLAVOR
 DOCKER_VOLUME_SIZE_GB=10
+KEYPAIR=${OS_USERNAME}-api-key
 
 # number of instances
 N_MASTER=1
@@ -9,6 +10,6 @@ N_NODES=1
 
 openstack coe cluster create --cluster-template k8s_cluster_template \
     --master-count $N_MASTER --node-count $N_NODES \
-    --keypair comet \
+    --keypair $KEYPAIR \
     --master-flavor $MASTER_FLAVOR --flavor $FLAVOR \
     --docker-volume-size $DOCKER_VOLUME_SIZE_GB k8s
