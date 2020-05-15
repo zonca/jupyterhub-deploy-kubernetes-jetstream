a121798de02691686dc7cd0b75577ef80fdfb505
Andrea Zonca <code@andreazonca.com>
2020-05-14 18:55:01 -0700

    feat: update the image to fedora atomic 29

--- kubernetes_magnum/create_template.sh
+++ kubernetes_magnum/create_template.sh
@@ -2,7 +2,7 @@ FLAVOR="m1.medium"
 MASTER_FLAVOR=$FLAVOR
 
 openstack coe cluster template create --coe kubernetes \
-    --image "Fedora-Atomic-27-20180419" \
+    --image "Fedora-Atomic-29" \
     --keypair comet \
     --external-network public --fixed-network ${OS_USERNAME}-newk8s-network --fixed-subnet ${OS_USERNAME}-newk8s-subnet1 --network-driver flannel \
     --flavor $FLAVOR --master-flavor $MASTER_FLAVOR \
