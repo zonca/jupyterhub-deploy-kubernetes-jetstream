4a77455fd4c39749e3ef3be33c83d4316221d756
Andrea Zonca <code@andreazonca.com>
2020-05-21 12:22:44 -0700

    use cdms secrets file

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,4 +4,4 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --version 0.9.0 \
-      --values config_standard_storage.yaml --values secrets.yaml
+      --values config_standard_storage.yaml --values cdms_secrets.yaml
