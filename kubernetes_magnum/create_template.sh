fd167e1ef4ea12cd25dc8ac335ce7d5d6614bc08
Andrea Zonca <code@andreazonca.com>
2020-07-09 16:44:40 -0700

    set image Fedora-Atomic-29-JS-Latest

--- kubernetes_magnum/create_template.sh
+++ kubernetes_magnum/create_template.sh
@@ -3,12 +3,13 @@ MASTER_FLAVOR=$FLAVOR
 DOCKER_VOLUME_SIZE_GB=10
 
 openstack coe cluster template create --coe kubernetes \
-    --image "Fedora-Atomic-29" \
+    --image "Fedora-Atomic-29-JS-Latest" \
     --keypair comet \
     --external-network public --fixed-network ${OS_USERNAME}-newk8s-network --fixed-subnet ${OS_USERNAME}-newk8s-subnet1 --network-driver flannel \
     --flavor $FLAVOR --master-flavor $MASTER_FLAVOR \
     --docker-volume-size $DOCKER_VOLUME_SIZE_GB --docker-storage-driver devicemapper \
     --floating-ip-enabled \
+    --server-type vm \
     --labels cloud-provider-enabled=true \
     --volume-driver cinder \
     k8s_cluster_template
