f8cc2a13240c529104c226ecc7be30c04557d401
Andrea Zonca <code@andreazonca.com>
2020-09-02 12:09:16 -0700

    add configuration for dask gateway to jupyterhub

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,4 +4,6 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --version 0.9.0 \
-      --values config_standard_storage.yaml --values cdms_secrets.yaml
+      --values config_standard_storage.yaml --values cdms_secrets.yaml \
+      --values dask_gateway/config_jupyterhub.yaml \
+      --values dask_gateway/config_jupyterhub_private.yaml
