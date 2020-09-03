7dc5bf9bc7d882c4d25d7c064eb676db5a761b76
Andrea Zonca <code@andreazonca.com>
2020-09-02 23:14:04 -0700

    store api token directly in config jupyterhub

--- install_jhub.sh
+++ install_jhub.sh
@@ -5,5 +5,4 @@ helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --version 0.9.0 \
       --values config_standard_storage.yaml --values cdms_secrets.yaml \
-      --values dask_gateway/config_jupyterhub.yaml \
-      --values dask_gateway/config_jupyterhub_private.yaml
+      --values dask_gateway/config_jupyterhub.yaml
