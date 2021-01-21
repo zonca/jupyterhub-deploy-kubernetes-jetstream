ffae7bd1322e9f39d992b5ff2edf74bc02a4c1c8
Andrea Zonca <code@andreazonca.com>
2021-01-21 09:03:09 -0800

    No resources goes to stderr anyway

--- hacktoscaler/5_get_number_of_singleuser_pods_in_node.sh
+++ hacktoscaler/5_get_number_of_singleuser_pods_in_node.sh
@@ -1,1 +1,1 @@
-kubectl -n jhub get pods -l "component=singleuser-server"  --field-selector spec.nodeName=$1 | grep -v "No resources" | wc -l
+kubectl -n jhub get pods -l "component=singleuser-server"  --field-selector spec.nodeName=$1 | wc -l
