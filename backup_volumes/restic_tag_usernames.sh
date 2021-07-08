fb027a838a83bdf3f8610bc0f33053988588928c
Andrea Zonca <code@andreazonca.com>
2021-07-08 01:26:11 -0700

    don't tag if the username file is absent

--- backup_volumes/restic_tag_usernames.sh
+++ backup_volumes/restic_tag_usernames.sh
@@ -1,6 +1,9 @@
 for id in $(restic -r $RESTIC_REPO snapshots --json | jq -r '.[]["id"]')
 do
+    echo "**** ID is $id ****"
     username=$(restic -r $RESTIC_REPO dump $id stash-data/.username)
-    restic -r $RESTIC_REPO tag --set $username $id
+    if [ $? -eq 0 ]; then
+        restic -r $RESTIC_REPO tag --set $username $id
+    fi
     echo $id $username
 done
