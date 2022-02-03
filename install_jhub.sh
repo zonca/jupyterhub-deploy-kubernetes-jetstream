b2aff12e45d01cf673b174b136b37aaa5c0fb876
Andrea Zonca <code@andreazonca.com>
2022-02-02 17:13:12 -0800

    upgrade to JupyterHub Helm 1.2.0

--- install_jhub.sh
+++ install_jhub.sh
@@ -3,6 +3,5 @@ NAMESPACE=jhub
 
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
-      --version 0.11.1 \
       --values config_standard_storage.yaml --values cdms_secrets.yaml \
-      --values dask_gateway/config_jupyterhub.yaml
+      --version 1.2.0 \
