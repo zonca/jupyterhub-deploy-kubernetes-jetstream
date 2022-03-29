212375b387b5496117d63c32986d476b418856c9
Andrea Zonca <code@andreazonca.com>
2022-03-29 15:19:45 -0700

    just tag the last 10

--- backup_volumes/restic_tag_usernames.sh
+++ backup_volumes/restic_tag_usernames.sh
@@ -1,9 +1,9 @@
-for id in $(restic -r $RESTIC_REPO snapshots --json | jq -r '.[]["id"]')
+for id in $(restic snapshots --latest 10 --json | jq -r '.[]["id"]')
 do
     echo "**** ID is $id ****"
-    username=$(restic -r $RESTIC_REPO dump $id stash-data/.username)
+    username=$(restic dump $id stash-data/.username)
     if [ $? -eq 0 ]; then
-        restic -r $RESTIC_REPO tag --set $username $id
+        restic tag --set $username $id
     fi
     echo $id $username
 done
