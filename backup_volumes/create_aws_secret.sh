12aed754e2cb33c9813afc3a40a069c40e2e5176
Andrea Zonca <code@andreazonca.com>
2022-04-27 15:48:57 -0700

    also save restic repository

--- backup_volumes/create_aws_secret.sh
+++ backup_volumes/create_aws_secret.sh
@@ -1,4 +1,5 @@
 kubectl create secret generic -n jhub s3-secret \
+    --from-file=./RESTIC_REPOSITORY \
     --from-file=./RESTIC_PASSWORD \
     --from-file=./AWS_ACCESS_KEY_ID \
     --from-file=./AWS_SECRET_ACCESS_KEY
