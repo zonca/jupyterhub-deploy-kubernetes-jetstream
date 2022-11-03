65efe17f2aaf729a3cc699bdb13d3623dfced99a
Andrea Zonca <code@andreazonca.com>
2022-11-02 22:19:59 -0700

    make helm more verbose

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,4 +4,5 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --version 1.2.0 \
+      --debug \
       --values config_standard_storage.yaml --values secrets.yaml
