94f3378ec959ed36e2491e6700462cfb0007ebe9
Andrea Zonca <code@andreazonca.com>
2022-11-30 15:04:25 -0800

    automatically create namespace

--- install_jhub.sh
+++ install_jhub.sh
@@ -3,6 +3,7 @@ NAMESPACE=jhub
 
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
+      --create-namespace \
       --version 1.2.0 \
       --debug \
       --values config_standard_storage.yaml --values secrets.yaml
