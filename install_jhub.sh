35c09bc5c8ef3c9245764396e4ce235d7d462d7b
Andrea Zonca <code@andreazonca.com>
2023-03-02 09:39:05 -0800

    switch to JupyterHub 2.0.0

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,6 +4,6 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --create-namespace \
-      --version 1.2.0 \
+      --version 2.0.0 \
       --debug \
       --values config_standard_storage.yaml --values secrets.yaml
