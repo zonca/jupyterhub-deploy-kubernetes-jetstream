c2a3e5e49848650fb238e3dda435d4d65486898f
Andrea Zonca <code@andreazonca.com>
2023-10-18 15:05:36 -0700

    update to dask
    see #65

--- dask_gateway/install_dask-gateway.sh
+++ dask_gateway/install_dask-gateway.sh
@@ -4,7 +4,7 @@ NAMESPACE=jhub
 helm upgrade  \
     --install \
     --repo=https://helm.dask.org \
-    --version 2022.4.0 \
+    --version 2023.9.0 \
     --namespace $NAMESPACE \
     --values config_dask-gateway.yaml \
     $RELEASE \
