200da5b38a947eb8f7ee3f5c717f9a726a7592b2
Andrea Zonca <code@andreazonca.com>
2024-12-13 17:10:16 -0800

    no need for network and template

--- kubernetes_magnum/create_network.sh
+++
@@ -1,9 +0,0 @@
-openstack network create ${OS_USERNAME}-newk8s-network
-
-openstack subnet create --network ${OS_USERNAME}-newk8s-network --subnet-range 10.0.0.0/24 ${OS_USERNAME}-newk8s-subnet1
-
-openstack router create ${OS_USERNAME}-newk8s-router
-
-openstack router add subnet ${OS_USERNAME}-newk8s-router ${OS_USERNAME}-newk8s-subnet1
-
-openstack router set --external-gateway public ${OS_USERNAME}-newk8s-router
