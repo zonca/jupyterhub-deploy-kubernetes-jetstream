d01ad7580e48a9f39eefec8d255aa15848781b3a
Andrea Zonca <code@andreazonca.com>
2024-12-31 12:40:36 -0800

    time creation of the cluster

--- kubernetes_magnum/create_cluster.sh
+++ kubernetes_magnum/create_cluster.sh
@@ -1,13 +1,19 @@
-# we can override the default values of the template
+#!/bin/bash
+
+# We can override the default values of the template
 FLAVOR="m3.small"
 TEMPLATE="kubernetes-1-30-jammy"
 MASTER_FLAVOR=$FLAVOR
 DOCKER_VOLUME_SIZE_GB=10
 
-# number of instances
-N_MASTER=3 # needs to be odd
+# Number of instances
+N_MASTER=3 # Needs to be odd
 N_NODES=1
 
+# Start timing
+START=$(date +%s)
+
+# Create the cluster
 openstack coe cluster create --cluster-template $TEMPLATE \
     --master-count $N_MASTER --node-count $N_NODES \
     --master-flavor $MASTER_FLAVOR --flavor $FLAVOR \
@@ -16,3 +22,28 @@ openstack coe cluster create --cluster-template $TEMPLATE \
     --labels min_node_count=1 \
     --labels max_node_count=5 \
     $K8S_CLUSTER_NAME
+
+# Poll the cluster status
+echo "Waiting for the cluster to become ready..."
+while true; do
+    STATUS=$(openstack coe cluster show $K8S_CLUSTER_NAME -f value -c status)
+    echo "Current status: $STATUS"
+    if [ "$STATUS" == "CREATE_COMPLETE" ]; then
+        break
+    elif [ "$STATUS" == "CREATE_FAILED" ]; then
+        echo "Cluster creation failed."
+        exit 1
+    fi
+    sleep 10
+done
+
+# End timing
+END=$(date +%s)
+DURATION=$((END - START))
+
+# Convert duration to minutes and seconds
+MINUTES=$((DURATION / 60))
+SECONDS=$((DURATION % 60))
+
+# Output the elapsed time
+echo "Cluster creation took $MINUTES minutes and $SECONDS seconds."
