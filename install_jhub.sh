b89629d00f57ed390e180e730f14b6b34cc897ce
ana espinoza <respinoza@ucar.edu>
2025-06-30 10:48:17 -0600

    JupyterHub Helm Chart v4.2.0

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,6 +4,6 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --create-namespace \
-      --version 3.0.3 \
+      --version 4.2.0 \
       --debug \
       --values config_standard_storage.yaml --values secrets.yaml
