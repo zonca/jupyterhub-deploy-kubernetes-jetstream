2362e8cc512eea3a7b484316fdc97d66b6d30c91
Andrea Zonca <code@andreazonca.com>
2025-11-17 19:39:05 -0800

    newer jhub

--- install_jhub.sh
+++ install_jhub.sh
@@ -4,6 +4,6 @@ NAMESPACE=jhub
 helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --namespace $NAMESPACE  \
       --create-namespace \
-      --version 4.2.0 \
+      --version 4.3.1 \
       --debug \
       --values config_standard_storage.yaml --values secrets.yaml
