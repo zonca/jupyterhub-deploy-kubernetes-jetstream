e36af178087cb0777e6975d066bdb96d73b84f34
Andrea Zonca <code@andreazonca.com>
2026-02-03 16:27:16 -0800

    Apply stashed updates after rebase

--- install_jhub.sh
+++ install_jhub.sh
@@ -6,4 +6,8 @@ helm upgrade --install $RELEASE jupyterhub/jupyterhub \
       --create-namespace \
       --version 4.3.1 \
       --debug \
-      --values config_standard_storage.yaml --values secrets.yaml
+      --values config_standard_storage.yaml \
+      --values dask_operator/jupyterhub_config.yaml \
+      --values dask_operator/jupyterhub_dask_dashboard_config.yaml \
+      --values secrets.yaml
+#      --values gpu/jupyterhub_gpu.yaml \
